b311b82bd93a5d0c3234bae7c77d8ee54b3f85b6
NathU <ulm08001@byui.edu>
2018-03-27 21:50:52 +0000

    fixed url

--- client/game_logic.js
+++ client/game_logic.js
@@ -1,7 +1,7 @@
 var vars = {
 	'bounce' : 1.03,
 	'friction' : 0.9,
-	'url' : "http://localhost:3001/",//"http://10.200.34.231:3001/";//
+	'url' : "https://www.joyfulnetworking.com/",//"http://localhost:3001/",//"http://10.200.34.231:3001/";//
 	'game_width' : 1200,
 	'game_height' : 540,
 	'refresh_interval' : (1000 / 50 /* <- FPS */)
