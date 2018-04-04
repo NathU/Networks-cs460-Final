779523ee3dd783666720e21bb74049b5a7e1ae06
Nathan Ulmer <ulm08001@byui.edu>
2018-04-04 17:21:27 -0600

    Delete environment.js

--- environment.js
+++
@@ -1,9 +0,0 @@
-exports.port = 3002; // server listens on this port
-
-exports.limit = 600; // connection pool count.
-exports.host='127.0.0.1';//where is your MySQL server running? (usually 127.0.0.1)
-exports.user= "root"; // your MySQL user for this application
-exports.password="jjsdatabase321"; // your MySQL password for the above user
-
-exports.database="game460";
-exports.path_to_dist="./client";
